a9840c2c5fc4dea573075b44915bb7c27511af96
tempcke <t.github@logikostech.net>
2017-11-17 12:46:07 -0600

    refactored Config to extend Registry to reduce duplication of basic things

--- src/Config.php
+++ src/Config.php
@@ -9,11 +9,11 @@ use Logikos\Util\Config\PathEval;
  * NOTICE: \Phalcon\Config will be much faster than this class and you are encouraged to use it
  * @see ../docs/config/README.md
  */
-abstract class Config implements \ArrayAccess, \Countable, \Iterator {
+abstract class Config extends Registry {
   private $locked   = false;
-  private $values   = [];
 
   public function __construct(array $arrayConfig = []) {
+    parent::__construct();
     foreach($arrayConfig as $key => $value)
       $this->offsetSet($key, $value);
 
@@ -40,76 +40,41 @@ abstract class Config implements \ArrayAccess, \Countable, \Iterator {
         function ($value) {
           return $this->hasToArray($value) ? $value->toArray() : $value;
         },
-        $this->values
+        $this->rawValues()
     );
   }
 
   private function hasToArray($value): bool {
-    return is_object($value) && method_exists($value, 'toArray');
+    return is_object($value)
+        && method_exists($value, 'toArray');
   }
 
   public function path($path, $default = null, $delimiter = '.') {
     return (new PathEval($this))->find($path, $default, $delimiter);
   }
 
-  # Countable
-  public function count() {
-    return count($this->values);
-  }
-
 
   # ArrayAccess
-  public function offsetExists($offset) {
-    return array_key_exists($offset, $this->values);
-  }
-
-  public function offsetGet($offset) {
-    if (!$this->offsetExists($offset))
-      throw new \OutOfBoundsException("offset '{$offset}' does not exist");
-    return $this->values[$offset];
-  }
-
   public function offsetSet($offset, $value) {
     $this->blockIfLocked();
-    $this->values[strval($offset)] = is_array($value)
-        ? new static($value)
-        : $value;
+    parent::offsetSet(
+        $offset,
+        is_array($value) ? new static($value) : $value
+    );
   }
 
   public function offsetUnset($offset) {
     $this->blockIfLocked();
-    unset($this->values[strval($offset)]);
+    parent::offsetUnset($offset);
   }
 
 
-  # Iterator
-  public function rewind()  { return reset($this->values);        }
-  public function key()     { return key($this->values);          }
-  public function current() { return current($this->values);      }
-  public function next()    { return next($this->values);         }
-  public function valid()   { return key($this->values) !== null; }
-
-
-  # Magic Property Access
-  public function __set($offset, $value) { $this->offsetSet($offset, $value);   }
-  public function __unset($offset)       { $this->offsetUnset($offset);         }
-  public function __get($offset)         { return $this->offsetGet($offset);    }
-  public function __isset($offset)       { return $this->offsetExists($offset); }
-
-  /**
-   * @param array $data
-   * @return static
-   */
   public static function __set_state(array $data): Config {
     return new static($data);
   }
 
   public function lock() {
     $this->locked = true;
-  }
-
-  protected function rawValues() {
-    return $this->values;
   }
 
   protected function isConfigObject($value) {
--- src/Registry.php
+++ src/Registry.php
@@ -11,124 +11,60 @@ namespace Logikos\Util;
  *
  *  // Set value
  *  $registry->something = 'something';
- *  // or
  *  $registry['something'] = 'something';
  *
  *  // Get value
  *  $value = $registry->something;
- *  // or
  *  $value = $registry['something'];
  *
  *  // Check if the key exists
  *  $exists = isset($registry->something);
- *  // or
  *  $exists = isset($registry['something']);
  *
  *  // Unset
  *  unset($registry->something);
- *  // or
  *  unset($registry['something']);
  *</code>
  */
 class Registry implements \ArrayAccess, \Countable, \Iterator {
-  protected $_data;
+  private $values;
 
-  /**
-   * Registry constructor
-   */
-  public function __construct() {
-    $this->_data = [];
+  public function __construct(array $data = []) {
+    $this->values = $data;
   }
 
-  /**
-   * Checks if the element is present in the registry
-   */
-  public function offsetExists($offset) {
-    return isset($this->_data[$offset]);
+  # Countable
+  public function count() {
+    return count($this->values);
   }
 
-  /**
-   * Returns an index in the registry
-   */
-  public final function offsetGet($offset) {
-    return $this->_data[$offset];
+  # ArrayAccess
+  public function offsetGet($offset)       { return $this->requireOffset($offset); }
+  public function offsetExists($offset)    { return isset($this->values[$offset]); }
+  public function offsetSet($offset, $val) { $this->values[$offset] = $val;        }
+  public function offsetUnset($offset)     { unset($this->values[$offset]);        }
+
+  # Iterator
+  public function rewind()  { return reset($this->values);        }
+  public function key()     { return key($this->values);          }
+  public function current() { return current($this->values);      }
+  public function next()    { return next($this->values);         }
+  public function valid()   { return key($this->values) !== null; }
+
+  # Magic Property Access
+  public function __set($offset, $value) { $this->offsetSet($offset, $value);   }
+  public function __unset($offset)       { $this->offsetUnset($offset);         }
+  public function __get($offset)         { return $this->offsetGet($offset);    }
+  public function __isset($offset)       { return $this->offsetExists($offset); }
+
+  protected function requireOffset($offset) {
+    if (!$this->offsetExists($offset))
+      throw new \OutOfBoundsException("offset '{$offset}' does not exist");
+
+    return $this->values[$offset];
   }
 
-  /**
-   * Sets an element in the registry
-   */
-  public final function offsetSet($offset, $value) {
-    $this->_data[$offset] = $value;
-  }
-
-  /**
-   * Unsets an element in the registry
-   */
-  public final function offsetUnset($offset) {
-    unset($this->_data[$offset]);
-  }
-
-  /**
-   * Checks how many elements are in the register
-   */
-  public final function count() {
-    return count($this->_data);
-  }
-
-  /**
-   * Moves cursor to next row in the registry
-   */
-  public final function next() {
-    next($this->_data);
-  }
-
-  /**
-   * Gets pointer number of active row in the registry
-   */
-  public final function key() {
-    return key($this->_data);
-  }
-
-  /**
-   * Rewinds the registry cursor to its beginning
-   */
-  public final function rewind() {
-    reset($this->_data);
-  }
-
-  /**
-   * Checks if the iterator is valid
-   */
-  public function valid() {
-    return key($this->_data) !== null;
-  }
-
-  /**
-   * Obtains the current value in the internal iterator
-   */
-  public function current() {
-    return current($this->_data);
-  }
-
-  /**
-   * Sets an element in the registry
-   */
-  public final function __set($key, $value) {
-    $this->offsetSet($key, $value);
-  }
-
-  /**
-   * Returns an index in the registry
-   */
-  public final function __get($key) {
-    return $this->offsetGet($key);
-  }
-
-  public final function __isset($key) {
-    return $this->offsetExists($key);
-  }
-
-  public final function __unset($key) {
-    $this->offsetUnset($key);
+  protected function rawValues() {
+    return $this->values;
   }
 }
