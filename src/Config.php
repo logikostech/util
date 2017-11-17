9670eae01654f6e84f308a332560d2afed527e30
tempcke <t.github@logikostech.net>
2017-11-17 12:05:52 -0600

    extracted PathEval out of Config to reduce complexity of Config object

--- src/Config.php
+++ src/Config.php
@@ -2,6 +2,8 @@
 
 namespace Logikos\Util;
 
+use Logikos\Util\Config\PathEval;
+
 /**
  * This is largely inspired by \Phalcon\Config - https://docs.phalconphp.com/hr/3.2/api/Phalcon_Config
  * NOTICE: \Phalcon\Config will be much faster than this class and you are encouraged to use it
@@ -47,56 +49,7 @@ abstract class Config implements \ArrayAccess, \Countable, \Iterator {
   }
 
   public function path($path, $default = null, $delimiter = '.') {
-    if ($this->pathStartsWithConfig($path, $delimiter))
-      return $this->evalSubPath($path, $delimiter, $default);
-
-    return $this->get($this->getFirstToken($path, $delimiter), $default);
-  }
-
-  private function pathStartsWithConfig($path, $delimiter) {
-    return $this->getFirstTokenValue($path, $delimiter) instanceof self;
-  }
-
-  private function getFirstTokenValue($path, $delimiter) {
-    return $this->get($this->getFirstToken($path, $delimiter));
-  }
-
-  private function getFirstToken($path, $delimiter) {
-    return $this->gettok($path, $delimiter,0);
-  }
-
-  private function evalSubPath($path, $delimiter, $default) {
-    return $this->getFirstTokenValue($path, $delimiter)->path(
-        $this->subtok($path, $delimiter, 1),
-        $default,
-        $delimiter
-    );
-  }
-
-  /**
-   * subtok(string, delimiter, offset, length)
-   *
-   * Usage:
-   *  subtok('a.b.c.d.e','.',0)     = 'a.b.c.d.e'
-   *  subtok('a.b.c.d.e','.',0,2)   = 'a.b'
-   *  subtok('a.b.c.d.e','.',2,1)   = 'c'
-   *  subtok('a.b.c.d.e','.',2,-1)  = 'c.d'
-   *  subtok('a.b.c.d.e','.',-4)    = 'b.c.d.e'
-   *  subtok('a.b.c.d.e','.',-4,2)  = 'b.c'
-   *  subtok('a.b.c.d.e','.',-4,-1) = 'b.c.d'
-   *
-   * @param  string   $string    The input string
-   * @param  string   $delimiter The boundary string
-   * @param  int      $offset    starting position, like in substr
-   * @param  int|null $length    length, like in substr
-   * @return string
-   */
-  private function subtok($string, $delimiter, $offset, $length = NULL) {
-    return implode($delimiter, array_slice(explode($delimiter, $string), $offset, $length));
-  }
-
-  private function gettok($string, $delimiter, $offset) {
-    return explode($delimiter, $string)[$offset];
+    return (new PathEval($this))->find($path, $default, $delimiter);
   }
 
   # Countable
