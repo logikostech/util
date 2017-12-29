e21e8771cb62e71bd02789a7c1cfe7aa6d45bef6
tempcke <t.bitbucket@logikostech.net>
2017-12-28 19:01:32 -0600

    added null or empty validator

---
+++ src/Config/Option/Validator/NotNullOrEmptyString.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Logikos\Util\Config\Option\Validator;
+
+use Logikos\Util\Config\Option\Validator;
+
+class NotNullOrEmptyString implements Validator {
+
+  public function validate($value) {
+    return !is_null($value) && $value !== '';
+  }
+
+  public function getMessage() {
+    return 'Value van not be null or empty string';
+  }
+}
---
+++ tests/Config/Option/Validator/NotNullOrEmptyStringTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Logikos\Util\Tests\Config\Option\Validator;
+
+
+use Logikos\Util\Config\Option\Validator\NotNullOrEmptyString;
+
+class NotNullOrEmptyStringTest extends TestCase {
+
+  public function testInvalidValues() {
+    $sut = new NotNullOrEmptyString();
+    $this->assertInvalid($sut, null);
+    $this->assertInvalid($sut, '');
+  }
+
+  public function testValidValues() {
+    $sut = new NotNullOrEmptyString();
+    $this->assertIsValid($sut, 0);
+    $this->assertIsValid($sut, ' ');
+    $this->assertIsValid($sut, "abc ABC 123 !@#$%^&*()_+-=[]{}\\|;:,.<>/?\n\r\t");
+  }
+}
