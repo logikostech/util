aa4651280aa7b000bce0806d378f58b7270eaf05
tempcke <t.bitbucket@logikostech.net>
2017-12-29 07:44:13 -0600

    modified to use Validator/TestCase and pulled some asserts up so they could be used by other tests

--- tests/Config/Option/Validator/PatternMatchTest.php
+++ tests/Config/Option/Validator/PatternMatchTest.php
@@ -2,20 +2,22 @@
 
 namespace Logikos\Util\Tests\Config\Option\Validator;
 
-use Logikos\Util\Config\Option\Validator;
 use Logikos\Util\Config\Option\Validator\PatternMatch;
 use Logikos\Util\Config\Option\Validator\ValidatorException;
-use Logikos\Util\Tests\Config\TestCase;
 
 class PatternMatchTest extends TestCase {
-  public function testImpl() {
+
+  public function testSetAndGetPattern() {
     $p = '/[a-zA-Z]{3}/';
     $v = new PatternMatch($p);
     $this->assertSame($p, $v->getPattern());
   }
 
   public function testInvalidPatternThrowsException() {
-    $this->assertInvalidPattern('abc');
+    $this->assertExceptionWillThrow(
+        ValidatorException::class,
+        function() { new PatternMatch('abc'); }
+    );
   }
 
   public function testValidValues() {
@@ -37,30 +39,5 @@ class PatternMatchTest extends TestCase {
     $message = 'Must be 3 consecutive letters';
     $validator = new PatternMatch('/^[a-zA-Z]{3}$/', $message);
     $this->assertSame($message, $validator->getMessage());
-  }
-
-
-
-  private function assertIsValid(Validator $validator, $value, $message = null) {
-    $this->assertTrue(
-        $validator->validate($value),
-        $message ?? 'Expected value to be invalid, but it was valid'
-    );
-  }
-
-  private function assertInvalid(Validator $validator, $value, $message = null) {
-    $this->assertFalse(
-        $validator->validate($value),
-        $message ?? 'Expected value to be valid, but it was not'
-    );
-  }
-
-  private function assertInvalidPattern($pattern) {
-    $this->assertExceptionWillThrow(
-        ValidatorException::class,
-        function() use($pattern) {
-          new PatternMatch($pattern);
-        }
-    );
   }
 }
