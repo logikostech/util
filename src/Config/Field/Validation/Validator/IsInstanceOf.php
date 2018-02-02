08539dc78a9623211212bd0525a98139fb6c360c
tempcke <t.bitbucket@logikostech.net>
2018-02-01 19:19:23 -0600

    added IsInstanceOf validator

--- src/Config/Field/Validation/Validator/IsInstanceOf.php
+++ src/Config/Field/Validation/Validator/IsInstanceOf.php
@@ -2,7 +2,38 @@
 
 namespace Logikos\Util\Config\Field\Validation\Validator;
 
+use Logikos\Util\Config\Field\Validation\Validator;
 
-class IsInstanceOf {
+class IsInstanceOf implements Validator {
+  private $fqcn;
+  private $description;
 
+  /**
+   * @param string $fqcn
+   * @param string $description
+   * @throws Exception
+   */
+  public function __construct($fqcn, $description = null) {
+    if (!$this->isValidClassName($fqcn))
+      throw new Exception("Invalid Regex pattern: {$fqcn}");
+
+    $this->fqcn = $fqcn;
+    $this->description = $description ?? "Must be an instance of {$fqcn}";
+  }
+
+  public function getFqcn() {
+    return $this->fqcn;
+  }
+
+  public function validate($value) : bool {
+    return is_a($value, $this->fqcn);
+  }
+
+  protected function isValidClassName($fqcn) {
+    return class_exists($fqcn);
+  }
+
+  public function getDescription() {
+    return $this->description;
+  }
 }
---
+++ tests/Config/Field/Validation/Validator/IsInstanceOfTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Logikos\Util\Tests\Config\Field\Validation\Validator;
+
+use Logikos\Util\Config\Field\Validation\Validator;
+
+class IsInstanceOfTest extends TestCase {
+  public function testInstantiate() {
+    $fqcn = \stdClass::class;
+    $v = new Validator\IsInstanceOf($fqcn);
+    $this->assertSame($fqcn, $v->getFQCN());
+  }
+
+  public function testInvalidClassNameThrowsException() {
+    $this->assertExceptionWillThrow(
+        Validator\Exception::class,
+        function() { new Validator\IsInstanceOf("Invalid\\ClassName"); }
+    );
+  }
+
+  public function testValidValues() {
+    $validator = new Validator\IsInstanceOf(\stdClass::class);
+    $this->assertIsValid($validator, new \stdClass);
+  }
+
+  public function testInvalidValues() {
+    $validator = new Validator\IsInstanceOf(\stdClass::class);
+    $this->assertInvalid($validator, 'string');
+    $this->assertInvalid($validator, '');
+    $this->assertInvalid($validator, 123);
+    $this->assertInvalid($validator, null);
+    $this->assertInvalid($validator, new \DateTime('now'));
+  }
+
+  public function testGetMessageAfterValidationFails() {
+    $message = "Hey! That isn't the right type!";
+    $validator = new Validator\IsInstanceOf(\stdClass::class, $message);
+    $this->assertSame($message, $validator->getDescription());
+  }
+
+  public function testGetDefaultMessage() {
+    $message = "Must be an instance of ".\stdClass::class;
+    $validator = new Validator\IsInstanceOf(\stdClass::class, $message);
+    $this->assertSame($message, $validator->getDescription());
+  }
+
+}
