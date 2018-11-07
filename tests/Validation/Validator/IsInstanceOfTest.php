167f6d78b38fd416ecfadaee5404a632cd6580d9
tempcke <t.bitbucket@logikostech.net>
2018-11-07 06:19:06 -0600

    cleanup

--- tests/Validation/Validator/IsInstanceOfTest.php
+++ tests/Validation/Validator/IsInstanceOfTest.php
@@ -5,10 +5,12 @@ namespace LogikosTest\Util\Validation\Validator;
 use Logikos\Util\Validation\Validator;
 
 class IsInstanceOfTest extends TestCase {
+  // Fully Qualified Class Name
+  const FQCN = \stdClass::class;
+
   public function testInstantiate() {
-    $fqcn = \stdClass::class;
-    $v = new Validator\IsInstanceOf($fqcn);
-    $this->assertSame($fqcn, $v->getFQCN());
+    $v = new Validator\IsInstanceOf(self::FQCN);
+    $this->assertSame(self::FQCN, $v->getFQCN());
   }
 
   public function testInvalidClassNameThrowsException() {
@@ -19,12 +21,12 @@ class IsInstanceOfTest extends TestCase {
   }
 
   public function testValidValues() {
-    $validator = new Validator\IsInstanceOf(\stdClass::class);
+    $validator = new Validator\IsInstanceOf(self::FQCN);
     $this->assertIsValid($validator, new \stdClass);
   }
 
   public function testInvalidValues() {
-    $validator = new Validator\IsInstanceOf(\stdClass::class);
+    $validator = new Validator\IsInstanceOf(self::FQCN);
     $this->assertInvalid($validator, 'string');
     $this->assertInvalid($validator, '');
     $this->assertInvalid($validator, 123);
@@ -34,13 +36,13 @@ class IsInstanceOfTest extends TestCase {
 
   public function testGetMessageAfterValidationFails() {
     $message = "Hey! That isn't the right type!";
-    $validator = new Validator\IsInstanceOf(\stdClass::class, $message);
+    $validator = new Validator\IsInstanceOf(self::FQCN, $message);
     $this->assertSame($message, $validator->getDescription());
   }
 
   public function testGetDefaultMessage() {
-    $message = "Must be an instance of ".\stdClass::class;
-    $validator = new Validator\IsInstanceOf(\stdClass::class, $message);
+    $message = "Must be an instance of ". self::FQCN;
+    $validator = new Validator\IsInstanceOf(self::FQCN, $message);
     $this->assertSame($message, $validator->getDescription());
   }
 
