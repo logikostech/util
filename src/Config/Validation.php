00d48c5d07a4a68d9da59a28e266fca55087c74d
tempcke <t.bitbucket@logikostech.net>
2018-11-15 20:51:32 -0600

    started working on Config/Validation ... left it failing

--- src/Config/Validation.php
+++ src/Config/Validation.php
@@ -3,9 +3,16 @@
 
 namespace Logikos\Util\Config;
 
+use Logikos\Util\Config;
 use Logikos\Util\Validation\Result;
 use Logikos\Util\Validation\Validator;
+use Logikos\Util\Validation\ValidResult;
 
 class Validation {
-  public function addFieldValidator($fieldgit)
+  public function validate(Config $config): Result {
+    return new ValidResult();
+  }
+  public function addFieldValidators($fieldName, Validator ...$validators) {
+
+  }
 }
--- tests/Config/ValidationTest.php
+++ tests/Config/ValidationTest.php
@@ -3,12 +3,51 @@
 
 namespace LogikosTest\Util\Config;
 
-
+use Logikos\Util\Config\MutableConfig as Config;
+use Logikos\Util\Config\Validation as ConfigValidation;
+use Logikos\Util\Validation;
+use Logikos\Util\Validation\Validator;
 use LogikosTest\Util\ValidationTestingTrait;
 
 class ValidationTest extends TestCase {
   use ValidationTestingTrait;
 
+  /** @var  ConfigValidation */
+  protected $validation;
 
+  public function setUp() {
+    $this->validation = new ConfigValidation;
+  }
+
+  public function testWhenNoValidatorsSet_ThenIsValid() {
+    $conf = new Config(['foo'=>'bar']);
+    $this->assertConfigIsValid($conf);
+  }
+
+  public function testAddFieldValidation() {
+    $conf = new Config();
+    $this->validation->addFieldValidators(
+        'age',
+        new Validator\Callback('is_int', 'Must be an integer')
+    );
+    $conf->age = 'ten';
+    $this->assertConfigIsNotValid($conf);;
+    $conf->age = 50;
+    $this->assertConfigIsValid($conf);
+  }
+
+  private function assertConfigIsValid(Config $conf) {
+    $this->assertTrue(
+        $this->validation->validate($conf)->isValid(),
+        "Expected config to be valid but it was invalid"
+    );
+  }
+
+  private function assertConfigIsNotValid(Config $conf) {
+    $this->assertFalse(
+        $this->validation->validate($conf)->isValid(),
+        "Expected config to be invalid but it was valid"
+    );
+  }
 
 }
